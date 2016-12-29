52949e5f246b6ccfa344ad90ec259afe82d4fd2b
Zhongxia Yan <zxyan@berkeley.edu>
2016-12-28 23:32:55 -0800

    Changed convolve functions to take in 4d arrays instead of 3d arrays

--- convolve.pyx
+++ convolve.pyx
@@ -6,29 +6,31 @@ cimport cython
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-def conv_forward(np.ndarray[np.float_t, ndim=3] input, np.ndarray[np.float_t, ndim=3] W, int dim_input, int dim_W, int padding, int stride, np.ndarray[np.float_t, ndim=3] output):
+def conv_forward(np.ndarray[np.float_t, ndim=4] input, np.ndarray[np.float_t, ndim=4] W, int padding, int stride, np.ndarray[np.float_t, ndim=4] output):
     '''
-    input: <batch_size> * <input_depth> * <dim_input ^ 2>
-    W: <output_depth> * <input_depth> * <dim_W ^ 2>
+    input: <batch_size> * <input_depth> * <dim_input> * <dim_input>
+    W: <output_depth> * <input_depth> * <dim_W> * <dim_W>
     padding: padding on left, right, top, and bottom
-    output: <batch_size> * <output_depth> * <dim_output ^ 2>
+    output: <batch_size> * <output_depth> * <dim_output> * <dim_output>
     '''
     output.fill(0)
     cdef int batch_size = input.shape[0]
     cdef int output_depth = output.shape[1]
     cdef int input_depth = input.shape[1]
-    cdef int dim_output = (dim_input - dim_W + 2 * padding) // stride + 1
+    cdef int dim_input = input.shape[2]
+    cdef int dim_output = output.shape[2]
+    cdef int dim_W = W.shape[2]
     cdef int m, n # indices in output
     cdef int i, j # indices in input
     cdef int a, b # indices in W
     cdef int ia, jb # temp variables
     cdef int x, y, z # respectively, indices in batch_size, output_depth, input_depth
     # memory views for efficient accessing
-    cdef np.float_t [:, :, :] input_buffer = input
-    cdef np.float_t [:, :, :] output_buffer = output
-    cdef np.float_t [:, :, :] W_buffer = W
-    cdef np.float_t [:, :] input_x, output_x, W_y
-    cdef np.float_t [:] input_xz, output_xy, W_yz
+    cdef np.float_t [:, :, :, :] input_buffer = input
+    cdef np.float_t [:, :, :, :] output_buffer = output
+    cdef np.float_t [:, :, :, :] W_buffer = W
+    cdef np.float_t [:, :, :] input_x, output_x, W_y
+    cdef np.float_t [:, :] input_xz, output_xy, W_yz
     cdef np.float_t output_xymn
     for x in range(batch_size):
         input_x = input_buffer[x]
@@ -41,47 +43,47 @@ def conv_forward(np.ndarray[np.float_t, ndim=3] input, np.ndarray[np.float_t, nd
                 input_xz = input_x[z]
                 for m in range(dim_output):
                     i = m * stride - padding
-                    for n in range(dim_output):
-                        j = n * stride - padding
-                        output_xymn = 0
-                        for a in range(dim_W):
-                            ia = (i + a)
-                            if ia < 0 or ia >= dim_input:
-                                continue
+                    for a in range(dim_W):
+                        ia = (i + a)
+                        if ia < 0 or ia >= dim_input:
+                            continue
+                        for n in range(dim_output):
+                            output_xymn = 0
+                            j = n * stride - padding
                             for b in range(dim_W):
                                 jb = (j + b)
                                 if jb < 0 or jb >= dim_input:
                                     continue
-                                output_xymn += input_xz[ia * dim_input + jb] * W_yz[a * dim_W + b]
-                        output_xy[m * dim_output + n] += output_xymn
+                                output_xy[m, n] += input_xz[ia, jb] * W_yz[a, b]
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-def conv_backward_W(np.ndarray[np.float_t, ndim=3] dJ_dout, np.ndarray[np.float_t, ndim=3] input, int dim_input, int dim_W, int padding, int stride, np.ndarray[np.float_t, ndim=3] dJ_dW):
+def conv_backward_W(np.ndarray[np.float_t, ndim=4] dJ_dout, np.ndarray[np.float_t, ndim=4] input, int padding, int stride, np.ndarray[np.float_t, ndim=4] dJ_dW):
     '''
-    dJ_dout: <batch_size> * <output_depth> * <dim_input ^ 2>
-    input: <batch_size> * <input_depth> * <dim_input ^ 2>
-    dJ_dW: <output_depth> * <input_depth> * <dim_W ^ 2>
+    dJ_dout: <batch_size> * <output_depth> * <dim_output> * <dim_output>
+    input: <batch_size> * <input_depth> * <dim_input> * <dim_input>
+    dJ_dW: <output_depth> * <input_depth> * <dim_W> * <dim_W>
     '''
     dJ_dW.fill(0)
     cdef int batch_size = input.shape[0]
     cdef int output_depth = dJ_dout.shape[1]
     cdef int input_depth = input.shape[1]
-    cdef int dim_output = (dim_input - dim_W + 2 * padding) // stride + 1
+    cdef int dim_input = input.shape[2]
+    cdef int dim_output = dJ_dout.shape[2]
+    cdef int dim_W = dJ_dW.shape[2]
     cdef int m, n # indices in output
     cdef int i, j # indices in input
     cdef int a, b # indices in W
     cdef int ia, jb # temp variables
     cdef int x, y, z # respectively, indices in batch_size, output_depth, input_depth
     # memory views for efficient accessing
-    cdef np.float_t [:, :, :] dJ_dout_buffer = dJ_dout
-    cdef np.float_t [:, :, :] input_buffer = input
-    cdef np.float_t [:, :, :] dJ_dW_buffer = dJ_dW
-    cdef np.float_t [:, :] dJ_dout_x, input_x, dJ_dW_y
-    cdef np.float_t [:] input_xz, dJ_dout_xy, dJ_dW_yz
-    cdef np.float_t dJ_dW_yzab
+    cdef np.float_t [:, :, :, :] dJ_dout_buffer = dJ_dout
+    cdef np.float_t [:, :, :, :] input_buffer = input
+    cdef np.float_t [:, :, :, :] dJ_dW_buffer = dJ_dW
+    cdef np.float_t [:, :, :] dJ_dout_x, input_x, dJ_dW_y
+    cdef np.float_t [:, :] input_xz, dJ_dout_xy, dJ_dW_yz
 
     for x in range(batch_size):
         dJ_dout_x = dJ_dout_buffer[x]
@@ -93,31 +95,36 @@ def conv_backward_W(np.ndarray[np.float_t, ndim=3] dJ_dout, np.ndarray[np.float_
                 dJ_dW_yz = dJ_dW_y[z]
                 input_xz = input_x[z]
                 for a in range(dim_W):
-                    for b in range(dim_W):
-                        dJ_dW_yzab = 0
-                        for m in range(dim_output):
-                            i = m * stride - padding
-                            ia = (i + a)
-                            if ia < 0 or ia >= dim_input:
-                                continue
+                    for m in range(dim_output):
+                        i = m * stride - padding
+                        ia = (i + a)
+                        if ia < 0 or ia >= dim_input:
+                            continue
+                        for b in range(dim_W):
                             for n in range(dim_output):
                                 j = n * stride - padding
                                 jb = (j + b)
                                 if jb < 0 or jb >= dim_input:
                                     continue
-                                dJ_dW_yzab += input_xz[ia * dim_input + jb] * dJ_dout_xy[m * dim_output + n]
-                        dJ_dW_yz[a * dim_W + b] += dJ_dW_yzab
+                                dJ_dW_yz[a, b] += input_xz[ia, jb] * dJ_dout_xy[m, n]
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-def conv_backward_input(np.ndarray[np.float_t, ndim=3] dJ_dout, np.ndarray[np.float_t, ndim=3] W, int dim_input, int dim_W, int padding, int stride, np.ndarray[np.float_t, ndim=3] dJ_din):
+def conv_backward_input(np.ndarray[np.float_t, ndim=4] dJ_dout, np.ndarray[np.float_t, ndim=4] W, int padding, int stride, np.ndarray[np.float_t, ndim=4] dJ_din):
+    '''
+    dJ_dout: <batch_size> * <output_depth> * <dim_output> * <dim_output>
+    W: <output_depth> * <input_depth> * <dim_W> * <dim_W>
+    dJ_din: <batch_size> * <input_depth> * <dim_input> * <dim_input>
+    '''
     dJ_din.fill(0)
     cdef int batch_size = dJ_din.shape[0]
     cdef int output_depth = dJ_dout.shape[1]
     cdef int input_depth = dJ_din.shape[1]
-    cdef int dim_output = (dim_input - dim_W + 2 * padding) // stride + 1
+    cdef int dim_input = dJ_din.shape[2]
+    cdef int dim_output = dJ_dout.shape[2]
+    cdef int dim_W = W.shape[2]
     cdef int m, n # indices in output
     cdef int i, j # indices in input
     cdef int a, b # indices in W
@@ -126,12 +133,11 @@ def conv_backward_input(np.ndarray[np.float_t, ndim=3] dJ_dout, np.ndarray[np.fl
     cdef int m_iter, n_iter, a_iter, b_iter # iteration variables
     cdef int x, y, z # respectively, indices in batch_size, output_depth, input_depth
     # memory views for efficient accessing
-    cdef np.float_t [:, :, :] dJ_dout_buffer = dJ_dout
-    cdef np.float_t [:, :, :] dJ_din_buffer = dJ_din
-    cdef np.float_t [:, :, :] W_buffer = W
-    cdef np.float_t [:, :] dJ_dout_x, dJ_din_x, W_y
-    cdef np.float_t [:] dJ_dout_xy, W_yz, dJ_din_xz
-    cdef np.float_t dJ_din_xzij
+    cdef np.float_t [:, :, :, :] dJ_dout_buffer = dJ_dout
+    cdef np.float_t [:, :, :, :] dJ_din_buffer = dJ_din
+    cdef np.float_t [:, :, :, :] W_buffer = W
+    cdef np.float_t [:, :, :] dJ_dout_x, dJ_din_x, W_y
+    cdef np.float_t [:, :] dJ_dout_xy, W_yz, dJ_din_xz
 
     for x in range(batch_size):
         dJ_dout_x = dJ_dout_buffer[x]
@@ -156,16 +162,14 @@ def conv_backward_input(np.ndarray[np.float_t, ndim=3] dJ_dout, np.ndarray[np.fl
                         if n >= dim_output:
                             b += stride * (n - dim_output + 1)
                             n = dim_output - 1
-                        dJ_din_xzij = 0
                         m_iter = m
                         a_iter = a
                         while m_iter >= 0 and a_iter < dim_W:
                             n_iter = n
                             b_iter = b
                             while n_iter >= 0 and b_iter < dim_W:
-                                dJ_din_xzij += dJ_dout_xy[m_iter * dim_output + n_iter] * W_yz[a_iter * dim_W + b_iter]
+                                dJ_din_xz[i, j] += dJ_dout_xy[m_iter, n_iter] * W_yz[a_iter, b_iter]
                                 n_iter -= 1
                                 b_iter += stride
                             m_iter -= 1
                             a_iter += stride
-                        dJ_din_xz[i * dim_input + j] += dJ_din_xzij
